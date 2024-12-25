d927961817883b591c83d2179247a0f858f06460
abla-rabia <la_rabia@esi.dz>
2024-12-25 02:51:36 +0100

    finished navbar

--- View/homeView.php
+++ View/homeView.php
@@ -8,18 +8,57 @@ class homeView{
             <title>El Mountada</title>
             <link rel="stylesheet" href="View/css/homeStyle.css">
             <link rel="preconnect" href="https://fonts.googleapis.com">
+            <script src="View/scripts/homeScript.js"></script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         </head>
         <?php   
     }
+    public function button($content){
+        ?>
+        <button id="boutton">Se Connecter</button>
+        <?php
+    }
+    public function navBarD(){
+        ?>
+        <nav>
+            <a href="#"><img src="View/assets/logo.png" alt="logo light mode" width="120px"></a>
+            <a href="">Accueil</a>
+            <a href="">Partenaires</a>
+            <a href="">Offres</a>
+            <a href="">Rejoingez nous</a>
+            <li id="subMenuP">
+                Dons & bénévolats
+                <ul id="subMenu">
+                    <a>Demande d'aide</a>
+                    <a>Bénévolat</a>
+                    <a>Faire un don</a>
+                </ul>
+            </li>
+            <?php
+            $this->button("Se Connecter")
+            ?>
+        </nav>
+        
+        <?php
+    }
     public function slider(){
         ?>
         <div class="sliderImg">
-            <div class="text">
-                <h2>Oceanview Hotel News</h2>
-                <p>Our Winter Getaway Package is here! Enjoy 20% off, free breakfast, and access to the rooftop pool. Book now for stays from Dec 15 to Feb 28!</p>
+            <div class="header">
+                <img src="View/assets/logowhite.png" alt="logo light mode">
+                <div class="socialMedia">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                </div>
+            </div>
+            <div class="text" id="textContainer">
+                <h2 id="title">Oceanview Hotel News</h2>
+                <p id="content">Our Winter Getaway Package is here! Enjoy 20% off, free breakfast, and access to the rooftop pool. Book now for stays from Dec 15 to Feb 28!</p>
             </div>
             <div class="barres">
                 <h3>_</h3>
@@ -39,6 +78,7 @@ class homeView{
             <body>
             <?php
                 $this->slider();
+                $this->navBarD();
             ?>
             </body>
     
@@ -48,3 +88,4 @@ class homeView{
     }
 }
 ?>
+
---
+++ View/scripts/homeScript.js
@@ -0,0 +1,55 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    //changement du slider 
+    let titlesTable = ["Title one", "Title two", "Title three"];
+    let contentTable = ["Our Winter Getaway Package is here! Enjoy 20% off, free breakfast, and access to the rooftop pool. Book now for stays from Dec 15 to Feb 28!", "Our Winter Getaway Package is here! Enjoy 20% off, free breakfast, and access to the rooftop pool. Book now for stays from Dec 15 to Feb 28!", "Our Winter Getaway Package is here! Enjoy 20% off, free breakfast, and access to the rooftop pool. Book now for stays from Dec 15 to Feb 28!"];
+    let bgSrcTable = ["View/assets/slider1.png", "View/assets/slider2.png", "View/assets/slider3.png"];
+    let index = 0;
+    
+    
+    setInterval(() => {
+        let dotsDiv = document.getElementsByClassName("barres")[0]
+        let dots = dotsDiv.getElementsByTagName("h3");
+        let title = document.getElementById("title")
+        let content = document.getElementById("content")
+        let text = document.getElementById("textContainer")
+        let imgDiv = document.getElementsByClassName("sliderImg")[0]
+        setTimeout(() => {
+            text.classList.add('dissolve');  
+            setTimeout(() => {
+                dots[index].style.opacity = "0.4";
+                index++;
+                index = index % 3;
+                console.log(index)
+                text.classList.remove('dissolve');  
+                imgDiv.style.backgroundImage = `url(${bgSrcTable[index]})`;
+                title.innerText = titlesTable[index];
+                dots[index].style.opacity = "1";
+                content.innerText = contentTable[index];
+
+            }, 1000);
+        }, 1000);
+    }, 4000);
+
+    //pour la sticky nav bar,ajouter la classe sticky au nav lorsque on atteint le top de la page 
+    window.onscroll = function() {
+        let nav = document.querySelector('nav');
+        let imgDiv = document.getElementsByClassName("sliderImg")[0]
+        let heroHeight = imgDiv.offsetHeight;
+    
+        if (window.scrollY >= heroHeight) {
+            nav.classList.add('sticky');  // Ajoute la classe sticky quand on dépasse la hauteur de .hero
+            nav.style.borderRadius = "0";
+        } else {
+            nav.classList.remove('sticky');  // Retire la classe sticky quand on est en haut
+            nav.style.borderRadius = "16px 16px 0px 0px";
+        }
+    };
+    document.getElementById("subMenuP").addEventListener("mouseover", function(){
+        document.getElementById("subMenu").style.display = "flex";
+    })
+    document.getElementById("subMenuP").addEventListener("mouseleave", function(){
+        document.getElementById("subMenu").style.display = "none";
+    })
+
+})
